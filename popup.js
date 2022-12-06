40d8fdb340dcf67bbfe07b06cd0d0093ff7b30cf
Scott Sinclair <scott.sinclair@rollerdigital.com>
2022-12-06 14:56:55 +1100

    add import/export and a few niceties

--- popup.js
+++ popup.js
@@ -23,18 +23,51 @@ function saveData() {
 
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function deleteAllRows() {
+    var body = document.querySelector('tbody');
+    body.replaceWith(document.createElement('tbody'));
+}
+
 function addRow(accid, colour) {
     const table = document.querySelector('#accountlist>tbody');
     var row = table.insertRow();
-    var a = row.insertCell();
-    a.innerHTML = '<input class="accid" type="text" />';
-    var b = row.insertCell();
-    b.innerHTML = '<td><input class="colour" type="color" value="#e6194b"></td>';
 
-    if (accid) {
-        a.firstChild.value = accid;
-        b.firstChild.value = colour;
-    }
+    var cell = row.insertCell();
+    cell.innerHTML = '<input class="accid" type="text" />';
+    if (accid) cell.firstChild.value = accid;
+
+    var randomColour = getRandomColor();
+
+    var cell = row.insertCell();
+    cell.innerHTML = '<input class="colourtext" type="text" maxlength="7" value="' + randomColour + '">';
+    if (accid) cell.firstChild.value = colour;
+    cell.addEventListener('input', function(event) {
+        var colourField = cell.parentElement.getElementsByClassName('colour')[0];
+        colourField.value = event.target.value;
+    });
+
+    var cell = row.insertCell();
+    cell.innerHTML = '<input class="colour" type="color" value="' + randomColour + '">';
+    if (accid) cell.firstChild.value = colour;
+    cell.addEventListener('input', function(event) {
+        var colourText = cell.parentElement.getElementsByClassName('colourtext')[0];
+        colourText.value = event.target.value;
+    });
+
+    var cell = row.insertCell();
+    cell.innerHTML = '<a id="removelink" href="#">Remove</a>'
+    cell.addEventListener('click', function() {
+        table.deleteRow(row.rowIndex-1);
+    })
 }
 
 document.querySelectorAll('input').forEach(input => {
@@ -52,6 +85,42 @@ document.addEventListener('DOMContentLoaded', function() {
     saveButton.addEventListener('click', function() {
         saveData();
     });
+
+    var importButton = document.getElementById('import');
+    importButton.addEventListener('click', function() {
+        var data = window.prompt('Paste a import string', '');
+
+        deleteAllRows();
+
+        var rows = data.split(',');
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i].split(';');
+            addRow(row[0], row[1]);
+        }
+        
+        saveData();
+    })
+
+    var exportButton = document.getElementById('export');
+    exportButton.addEventListener('click', function() {
+        var allItems = [];
+        for (const row of document.querySelectorAll('tbody>tr')) {
+            var accountId = row.getElementsByClassName('accid')[0].value.trim()
+            var colour = row.getElementsByClassName('colour')[0].value;
+    
+            if (accountId.length) {
+                allItems.push(accountId + ';' + colour);
+            }
+        }
+
+        var data = allItems.join(',');
+        navigator.clipboard.writeText(data).then(function () {
+            alert('Copied!');
+        }, function() {
+            alert('Use this as your export string:\n' + data);
+        })
+    })
+
     chrome.storage.sync.get(['accounts'], (results) => {
         if (Object.keys(results.accounts).length > 0) {
             for (const acc in results.accounts) {
